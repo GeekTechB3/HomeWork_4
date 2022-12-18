8dfd153c0e4c47d542cd3e5152bef269bf4114b0
Abubakr306060 <115799807+Abubakr306060@users.noreply.github.com>
2022-12-18 22:06:00 +0600

    Update dz4.py

--- dz4.py
+++ dz4.py
@@ -2,90 +2,61 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher.filters.state import StatesGroup, State
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
-from pytube import YouTube
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+from pytube import YouTube
 import config
-import logging
-import os
 
-bot = Bot(token=config.token)
-dp = Dispatcher(bot, storage=MemoryStorage())
-storage = MemoryStorage()
-logging.basicConfig(level=logging.INFO)
+bot = Bot(config.token)
+dp = Dispatcher(bot)
 
-audio_button = KeyboardButton('uxui')
-video_button = KeyboardButton('video')
+backend_button = KeyboardButton("/backend")
+frontend_button = KeyboardButton("/frontend")
+uxui_button = KeyboardButton("/uxui")
+android_button = KeyboardButton("/android")
+ios_button = KeyboardButton("/ios")
 
 buttons = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-buttons.add(audio_button)
-buttons.add(video_button)
+buttons.add(backend_button)
+buttons.add(frontend_button)
+buttons.add(uxui_button)
+buttons.add(android_button)
+buttons.add(ios_button)
 
-@dp.message_handler(commands=['start'])
-async def start(message: types.Message):
-    await message.answer(f"""Здраствуйте, {message.from_user.full_name}
-Я вам помогу скачать аудио или же видео с ютуба""", reply_markup=buttons)
 
-class DownloadAudio(StatesGroup):
-    download = State()
+@dp.message_handler(commands=["start"])
+async def start(message : types.Message):
+    await message.answer(f"Здравствуйте, {message.from_user.full_name}. Добро пожаловать в Geektech, если хотите узнать по больше нажмите Помощь или напишите /help", reply_markup=buttons)
 
-class DownloadVideo(StatesGroup):
-    download = State()
 
-def download(url, type):
-    yt = YouTube(url)
-    if type == "uxui":
-        yt.streams.filter(only_audio=True).first().download("audio", f"{yt.title}.mp3")
-        return f"{yt.title}.mp3"
-    elif type == "video":
-        yt.streams.filter(progressive=True, file_extension="mp4").first().download("video", f"{yt.title}.mp4")
-        return f"{yt.title}.mp4"
+@dp.message_handler(commands=["backend"])
+async def backend(message:types.Message):
+    await message.answer("Backend — это разработка бизнес-логики продукта (сайта или веб-приложения). Бэкенд отвечает за взаимодействие пользователя с внутренними данными, которые потом отображает фронтенд. Попросту говоря, это то, что скрыто от глаз пользователя и происходит вне его браузера и компьютера.", reply_markup=buttons)
+    await message.answer("Стоимость 10000 сом в месяц")
+    await message.answer("Обучение: 5 месяц")
 
-@dp.message_handler(text = ["Аудио", "аудио", "Audio", "audio"])
-async def audio(message: types.Message):
-    await message.answer("Отправьте ссылку на видео и я вам отправлю его в mp3")
-    await DownloadAudio.download.set()
+@dp.message_handler(commands=["frontend"])
+async def frontend(message:types.Message):
+    await message.answer("Frontend — это создание пользовательского интерфейса на клиентской стороне веб‑сайта или приложения. Это всё, что видит пользователь, когда открывает веб-страницу, и с чем он взаимодействует: кнопки, баннеры и анимация.", reply_markup=buttons)
+    await message.answer("Стоимость 10000 сом в месяц")
+    await message.answer("Обучение: 5 месяц")
 
-@dp.message_handler(text = ["Видео", "видео", "Video", "video"])
-async def video(message: types.Message):
-    await message.answer("Отправьте ссылку на видео в ютубе и я вам его отправлю")
-    await DownloadVideo.download.set()
+@dp.message_handler(commands=["uxui"])
+async def uxui(message:types.Message):
+    await message.answer("Uxui — это проектирование удобных, понятных и эстетичных пользовательских интерфейсов. Чтобы разобраться, какие задачи решает специалист в этой сфере, нужно понять, что такое UX и UI. UX — user experience — переводится на русский язык как «пользовательский опыт».", reply_markup=buttons)
+    await message.answer("Стоимость 10000 сом в месяц")
+    await message.answer("Обучение: 3 месяц")
 
-@dp.message_handler(state=DownloadAudio.download)
-async def download_audio(message: types.Message, state : FSMContext):
-    try:
-        title = download(message.text, "audio")
-        audio = open(f"audio/{title}", "rb")
-        await message.answer("Скачиваем файл ожидайте...")
-        try:
-            await message.answer("Все скачалось вот держи")
-            await bot.send_audio(message.chat.id, audio)
-        except:
-            await message.answer("Произошла ошибка, попробуйте позже")
-        os.remove(f'audio/{title}')
-        await state.finish()
-    except:
-        await message.answer("Неверная ссылка на видео")
-        await state.finish()
 
-@dp.message_handler(state=DownloadVideo.download)
-async def download_video(message: types.Message, state : FSMContext):
-    await message.answer("Скачиваем видео файл ожидайте...")
-    try:
-        title = download(message.text, "video")
-        video = open(f"video/{title}", "rb")
-        try:
-            await message.answer("Все скачалось вот держи")
-            await bot.send_video(message.chat.id, video)
-        except:
-            await message.answer("Произошла ошибка, попробуйте позже")
-        os.remove(f'video/{title}')
-        await state.finish()
-    except:
-        await message.answer("Неверная ссылка на видео")
-        await state.finish()
+@dp.message_handler(commands=["android"])
+async def android(message:types.Message):
+    await message.answer("Android разработка — создает мобильные приложения на операционной системе Android. Андроид-программист использует языки Java, Kotlin, C++, иногда Javascript. В 2019 году Google объявил Kotlin официальным и предпочтительным языком для Android-разработки.", reply_markup=buttons)
+    await message.answer("Стоимость 10000 сом в месяц")
+    await message.answer("Обучение: 7 месяц")
 
-@dp.message_handler()
-async def not_found(message: types.Message):
-    await message.reply("Я вас не понял")
 
+@dp.message_handler(commands=["ios"])
+async def ios(message:types.Message):
+    await message.answer("IOS — разработчик создаёт приложения не только для iPhone, но и для iPad, Apple Watch и других гаджетов, входящих в экосистему. Он не только пишет код и работает над интерфейсом, но и занимается поддержкой приложения, адаптацией его под разные модели устройств, тестированием и исправлением багов.", reply_markup=buttons)
+    await message.answer("Стоимость 14000 сом в месяц")
+    await message.answer("Обучение: 5 месяц")
 executor.start_polling(dp)
